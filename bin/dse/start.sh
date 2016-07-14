b05640c0cfb5a2819dbb51ad907ba8e5bf940f96
someJ <justin_wignall@hotmail.com>
2016-07-14 18:13:44 +0100

    Restart agent after sleep
    Attempting to remove manual step

--- bin/dse/start.sh
+++ bin/dse/start.sh
@@ -5,3 +5,7 @@ sudo service dse start
 
 echo "Starting the DataStax Agent"
 sudo service datastax-agent start
+
+# restart agent once opscenter up and running. Can do manually on each node
+sleep 60
+sudo service datastax-agent restart
